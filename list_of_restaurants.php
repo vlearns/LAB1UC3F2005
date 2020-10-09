46a4f2014ec7f3b997471d572e03a77dd649d128
vlearns <49580154+vlearns@users.noreply.github.com>
2020-10-09 10:26:24 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -19,6 +19,11 @@
         <th>Menara Standard Chartered, TPM</th>
         <th>03-22441234</th>
          </tr>
+           <?php
+           $connectionInfo = array("UID" => "apustudent", "pwd" => "{database12345!}", "Database" => "apulab2exercise", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:lab2exercisetp050193.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
          </table>
          </center>
      </body>
